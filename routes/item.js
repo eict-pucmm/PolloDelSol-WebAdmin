11a392dc18d59486a490316f2d9630e7ba51716b
ManuelEduardo <manueleduardo0320@gmail.com>
2019-06-13 08:11:02 -0400

    user can list and filter items

--- routes/item.js
+++ routes/item.js
@@ -2,12 +2,20 @@ const express = require('express');
 let app = express();
 
 app.get('/', function (req, res, next) {
+
+    let sql_query = '';
+    if (req.query.tipo == 'Todos' || !req.query.tipo)
+        sql_query = 'SELECT * FROM item;';
+    else
+        sql_query = `SELECT * FROM item WHERE tipo = '${req.query.tipo}'`;
+
     req.getConnection(function (error, conn) {
-        conn.query('SELECT * FROM item WHERE eliminado = 0', function (err, rows, fields) {
+        conn.query(sql_query, function (err, rows, fields) {
             if (err) {
                 req.flash('error', err);
                 res.render(
                     'item/list', {
+                        filtro_tipo: 'Todos',
                         title: 'Lista de items',
                         data: ''
                     }
@@ -15,6 +23,7 @@ app.get('/', function (req, res, next) {
             } else {
                 res.render(
                     'item/list', {
+                        filtro_tipo: req.query.tipo,
                         title: 'Lista de items',
                         data: rows
                     }
@@ -27,6 +36,7 @@ app.get('/', function (req, res, next) {
 app.get('/register', function (req, res, next) {
     res.render(
         'item/register', {
+            action: 'Registrar',
             id_item: '',
             nombre: '',
             descripcion: '',
@@ -43,23 +53,23 @@ app.post('/register', function(req, res, next){
     req.assert('precio', 'El precio no puede estar vacío').notEmpty();
 
     let errors = req.validationErrors()
+    let item = {id: '', nombre: '', descripcion: '', tipo: '', precio: ''};
     
     if( !errors ) {   //No errors were found.  Passed Validation!
         
-        let item = {
-            id: req.sanitize('id-item').escape().trim(),
-            nombre: req.sanitize('nombre').escape().trim(),
-            descripcion: req.sanitize('descripcion').escape().trim(),
-            tipo: req.sanitize('tipo-item').escape().trim(),
-            precio: req.sanitize('precio').escape().trim()
-        };
+        item.id = req.sanitize('id-item').escape().trim();
+        item.nombre = req.sanitize('nombre').escape().trim();
+        item.descripcion = req.sanitize('descripcion').escape().trim();
+        item.tipo = req.sanitize('tipo-item').escape().trim();
+        item.precio = req.sanitize('precio').escape().trim();
 
-        let sql_query = "INSERT INTO item VALUES ('" +
-            item.id + "', '" +
-            item.nombre + "', '" +
-            item.descripcion + "', " +
-            item.precio + ", '" +
-            item.tipo + "', 0);";
+        let sql_query = `INSERT INTO item VALUES (
+            '${item.id}',
+            '${item.nombre}',
+            '${item.descripcion}',
+            ${item.precio},
+            '${item.tipo}',
+            0);`;
         
         req.getConnection(function(error, conn) {
             conn.query(sql_query, function(err, result) {
@@ -67,6 +77,7 @@ app.post('/register', function(req, res, next){
                     req.flash('error', err);
                     res.render(
                         'item/register', {
+                            action: 'Registrar',
                             id_item: item.id,
                             nombre: item.nombre,
                             descripcion: item.descripcion,
@@ -78,6 +89,7 @@ app.post('/register', function(req, res, next){
                     req.flash('success', "Item registrado exitosamente!");
                     res.render(
                         'item/register', {
+                            action: 'Registrar',
                             id_item: '',
                             nombre: '',
                             descripcion: '',
@@ -91,17 +103,114 @@ app.post('/register', function(req, res, next){
     }
     else {
         req.flash('error', errors[0].msg);
-
         res.render(
             'item/register', {
-                id_item: '',
-                nombre: '',
-                descripcion: '',
-                precio: '',
-                tipo: ''
+                action: 'Registrar',
+                id_item: item.id,
+                nombre: item.nombre,
+                descripcion: item.descripcion,
+                precio: item.precio,
+                tipo: item.tipo
             }
         );
     }
-})
+});
+
+app.get('/edit/(:id_item)', function(req, res, next){
+    req.getConnection(function(error, conn) {
+        conn.query(`SELECT * FROM item WHERE id_item = '${req.params.id_item}';`, function(err, rows, fields) {
+            if(err) throw err;
+            
+            if (rows.length <= 0) {
+                req.flash('error', `No se encontro el item con Id = "${req.params.id_item}"`);
+                res.redirect('/item')
+            }
+            else {
+                res.render('item/register', {
+                    action: 'Modificar',
+                    id_item: rows[0].id_item,
+                    nombre: rows[0].nombre,
+                    descripcion: rows[0].descripcion,
+                    tipo: rows[0].tipo,
+                    precio: rows[0].precio                    
+                })
+            }            
+        })
+    })
+});
+
+app.post('/edit/(:id_item)', function(req, res, next){
+    req.assert('nombre', 'El nombre no puede estar vacío').notEmpty();
+    req.assert('descripcion', 'La descripcion no puede estar vacía').notEmpty();
+    req.assert('precio', 'El precio no puede estar vacío').notEmpty();
+
+    let errors = req.validationErrors()
+    let item = {id: '', nombre: '', descripcion: '', tipo: '', precio: ''};
+    
+    if( !errors ) {
+        
+        item.id = req.sanitize('id-item').escape().trim();
+        item.nombre = req.sanitize('nombre').escape().trim();
+        item.descripcion = req.sanitize('descripcion').escape().trim();
+        item.tipo = req.sanitize('tipo-item').escape().trim();
+        item.precio = req.sanitize('precio').escape().trim();
+
+        let sql_query = `UPDATE item SET 
+            nombre = '${item.nombre}', 
+            descripcion = '${item.descripcion}', 
+            tipo = '${item.tipo}', 
+            precio = ${item.precio} 
+            WHERE id_item = '${item.id}';`;
+        
+        req.getConnection(function(error, conn) {
+            conn.query(sql_query, function(err, result) {
+                if (err) {
+                    req.flash('error', err);
+                    res.render(
+                        `item/edit/${item.id}`, {
+                            action: 'Modificar',
+                            id_item: item.id,
+                            nombre: item.nombre,
+                            descripcion: item.descripcion,
+                            precio: item.precio,
+                            tipo: item.tipo
+                        }
+                    );
+                } else {
+                    req.flash('success', 'Item modificado satisfactoriamente');
+                    res.redirect('/item');
+                }
+            })
+        })
+    }
+    else {
+        req.flash('error', errors[0].msg);
+        res.render(
+            `item/edit/${item.id}`, {
+                action: 'Modificar',
+                id_item: item.id,
+                nombre: item.nombre,
+                descripcion: item.descripcion,
+                precio: item.precio,
+                tipo: item.tipo
+            }
+        );
+    }
+});
+
+app.delete('/delete/(:id)', function(req, res, next) {
+    
+    req.getConnection(function(error, conn) {
+        conn.query(`UPDATE item SET eliminado = 1 WHERE id_item = ${req.params.id_item}`, function(err, result) {
+            if (err) {
+                req.flash('error', err);
+                res.redirect('/item');
+            } else {
+                req.flash('success', `Se ha eliminado el item con Id = ${req.params.id_item}`);
+                res.redirect('/item');
+            }
+        })
+    })
+});
 
 module.exports = app;
--- views/public/scripts/register_item.js
+++ views/public/scripts/register_item.js
@@ -1,7 +1,13 @@
 let messageP = document.getElementById('result-message');
-let message = messageP.innerHTML;
+if (messageP) {
+    let message = messageP.innerHTML;
 
-if (message.includes('ER_DUP_ENTRY')) {
-    messageP.innerHTML = 'ERROR: Ya existe un item con el Id "' + 
-        message.substring(message.indexOf("'") + 1, message.indexOf("'", message.indexOf("'") + 1)) + '"';
+    if (message.includes('ER_DUP_ENTRY')) {
+        messageP.innerHTML = 'ERROR: Ya existe un item con el Id = "' + 
+            message.substring(message.indexOf("'") + 1, message.indexOf("'", message.indexOf("'") + 1)) + '"';
+    }
+}
+
+if (document.getElementById('reg-button').innerHTML === 'Modificar') {
+    document.getElementById('id-item').disabled = true;
 }
