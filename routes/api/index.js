4f50a188f4ad2e6e7b1e7b6149b4febe2ce4ed07
ManuelEduardo <manueleduardo0320@gmail.com>
2019-08-11 19:44:46 -0400

    User can change relacion punto-peso

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,16 +6,53 @@ const menuAPI       = require('./menu');
 const platoAPI      = require('./platodeldia');
 let app = express();
 
-
-
-app.get('/',(req, res) => {
-    console.log("API running");
-});
-
 app.use('/item', itemAPI);
 app.use('/empresa', empresaAPI);
 app.use('/empleado', empleadoAPI);
 app.use('/menu', menuAPI);
 app.use('/platodeldia', platoAPI);
+
+app.get('/',(req, res) => {
+    res.status(200).send({error: false, message: 'API working sucessfully'});
+});
+
+app.get('/puntos', (req, res) => {
+    
+    req.getConnection((error, conn) => {
+        if (!error) {
+            conn.query(`SELECT * FROM relacionpunto`, (err, rows, fields) => {
+                if (!err) {
+                    if (rows.length > 0) {
+                        res.status(200).send({error: false, valor_puntos: rows[0].pesos_por_punto});
+                    } else {
+                        res.status(204).send({error: false, message: 'Server request successful but data was not found'});
+                    }
+                } else {
+                    res.status(500).send({error: true, message: err});
+                }
+            });
+        } else {
+            res.status(500).send({error: true, message: error});
+        }
+    });
+});
+
+app.post('/puntos', (req, res) => {
+    
+    req.getConnection((error, conn) => {
+        if (!error) {
+            conn.query(`UPDATE relacionpunto SET pesos_por_punto = ?`, req.body.puntos, (err, result) => {
+                if (!err) {
+                    res.status(200).send({error: false, message: 'Puntos modificados'});
+                } else {
+                    res.status(500).send({error: true, message: err});
+                }
+            });
+        } else {
+            res.status(500).send({error: true, message: error});
+        }
+    });
+});
+
 
 module.exports = app;
--- routes/public/index.js
+++ routes/public/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const axios = require('axios');
 const config  = require('../../config')
 let app = express();
 
 app.get('/', (req, res) => {
-    if(config.loggedIn){
-        res.render('index');
-    }else {
+    if (config.loggedIn) {
+        axios.get(`${config.values.server.url}/api/puntos`)
+        .then(result => {
+            res.render('index', {
+                valor_puntos: result.data.valor_puntos,
+            });
+        })
+        .catch(err => console.log(err));
+    } else {
         res.redirect('/login')
     }
+});
+
+app.post('/puntos', (req, res) => {
+    axios.post(`${config.values.server.url}/api/puntos`, {puntos: req.body.puntos})
+    .then(result => res.redirect('/'))
+    .catch(err => console.log(err));
 });
 
 module.exports = app;
