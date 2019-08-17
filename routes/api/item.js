2019637b7ed5dc8919f2bdf0fce4e1458f072ef7
José C. Urena <20160138@ce.pucmm.edu.do>
2019-08-17 11:04:03 -0400

    items and combos registerable thru mssql

--- routes/api/item.js
+++ routes/api/item.js
@@ -1,56 +1,91 @@
 const express = require('express');
 const axios = require('axios');
 const url = require('../../config').values.server.url;
+const {poolPromise} = require('../../db')
+const sql = require('mssql')
 
 let app = express();
 
-app.get('/categories', (req, res) => {
-    let categorias = [], subcategorias = [];
+app.get('/categories', async (req, res) => {
     let sql_query = `SELECT * FROM categoria`;
-
+    let categorias = [], subcategorias = [];
     sql_query += req.query.nombre ? ` AND categoria.nombre = '${req.query.nombre}'` : ``;
     sql_query += req.query.id_categoria ? ` AND categoria.id_categoria = ${req.query.id_categoria}` : ``;
-    
-    req.getConnection((error, conn) => {
-        if (!error) {
-            conn.query(sql_query, (err, rows, fields) => {
-                if (!err) {
-                    if (rows.length > 0) {
-                        rows.forEach(cat => {
-                            if (cat.id_categoria_padre == null) {
-                                categorias.push(cat);
-                            } else {
-                                subcategorias.push(cat);
-                            }
-                        });
-                        res.status(200).send({error: false, categorias: categorias, subcategorias: subcategorias});
-                    } else {
-                        res.status(204).send({error: false, message: 'Server request successful but data was not found'});
-                    }
+    sql_query += ';'
+    try {
+        const pool = await poolPromise
+        const result = await pool.request()
+            .query(sql_query)
+            result.recordset.forEach(cat => {
+                if (cat.id_categoria_padre == null) {
+                    categorias.push(cat);
                 } else {
-                    res.status(500).send({error: true, message: err});
+                    subcategorias.push(cat);
                 }
             });
-        } else {
-            res.status(500).send({error: true, message: error});
-        }
-    });
+        res.status(200).send({error: false, categorias: categorias, subcategorias: subcategorias});
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({error: true, message: err});
+    } 
+
+
+    //Vainita MySQL
+    // let categorias = [], subcategorias = [];
+    // let sql_query = `SELECT * FROM categoria`;
+
+    // sql_query += req.query.nombre ? ` AND categoria.nombre = '${req.query.nombre}'` : ``;
+    // sql_query += req.query.id_categoria ? ` AND categoria.id_categoria = ${req.query.id_categoria}` : ``;
+    
+    // req.getConnection((error, conn) => {
+    //     if (!error) {
+    //         conn.query(sql_query, (err, rows, fields) => {
+    //             if (!err) {
+    //                 if (rows.length > 0) {
+    //                     rows.forEach(cat => {
+    //                         if (cat.id_categoria_padre == null) {
+    //                             categorias.push(cat);
+    //                         } else {
+    //                             subcategorias.push(cat);
+    //                         }
+    //                     });
+    //                     res.status(200).send({error: false, categorias: categorias, subcategorias: subcategorias});
+    //                 } else {
+    //                     res.status(204).send({error: false, message: 'Server request successful but data was not found'});
+    //                 }
+    //             } else {
+    //                 res.status(500).send({error: true, message: err});
+    //             }
+    //         });
+    //     } else {
+    //         res.status(500).send({error: true, message: error});
+    //     }
+    // });
 });
 
-app.get('/get', (req, res) => {
+app.get('/get', async (req, res) => {
 
     let sql_query = `SELECT item.id_item, item.nombre, item.descripcion, categoria.nombre AS categoria, subcategoria.nombre AS subcategoria, item.precio, item.eliminado, item.imagen 
         FROM item, categoria AS categoria, categoria AS subcategoria 
         WHERE item.id_categoria = categoria.id_categoria 
         AND item.id_subcategoria = subcategoria.id_categoria`;
-
     sql_query += req.query.id_item ? ` AND item.id_item = '${req.query.id_item}'` : ``;
     sql_query += req.query.nombre ? ` AND item.nombre = '${req.query.nombre}'` : ``;
     sql_query += req.query.categoria ? ` AND categoria.nombre = '${req.query.categoria}'` : ``;
     sql_query += req.query.subcategoria ? ` AND subcategoria.nombre = '${req.query.subcategoria}'` : ``;
     sql_query += req.query.eliminado ? ` AND item.eliminado = ${req.query.eliminado}` : ``;
 
-    req.getConnection((error, conn) => {
+    try {
+        const pool = await poolPromise
+        const result = await pool.request()
+            .query(sql_query)
+        res.status(200).send({error: false, items: result.recordset});
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({error: true, message: err});
+    } 
+    //Vainita MySQL
+    /*req.getConnection((error, conn) => {
         if (!error) {
             conn.query(sql_query, (err, rows, fields) => {
                 if (!err) {
@@ -66,15 +101,28 @@ app.get('/get', (req, res) => {
         }else{
             res.status(500).send({error: true, message: error});
         }
-    })
+    })*/
 });
 
-app.get('/get/combo', (req, res) => {
+app.get('/get/combo', async (req, res) => {
 
     let selectComboQuery = `SELECT * FROM combo WHERE id_combo = '${req.query.id_combo}'`;
     let selectItemComboQuery = `SELECT * FROM itemcombo WHERE id_combo = '${req.query.id_combo}'`;
 
-    req.getConnection((error, conn) => {
+    try {
+        const pool = await poolPromise
+        const comboresults = await pool.request()
+            .query(selectComboQuery)
+        const itemcomboresults = await pool.request()
+            .query(selectItemComboQuery);
+        res.status(200).send({error: false, combo: comboresults.recordset, itemCombo: itemcomboresults.recordset});
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({error: true, message: err});
+    } 
+    
+    //Vainita MySQL
+    /*req.getConnection((error, conn) => {
         if (!error) {
             conn.query(selectComboQuery, (err, combo, fields) => {
                 if (!err) {
@@ -100,16 +148,37 @@ app.get('/get/combo', (req, res) => {
         } else {
             res.status(500).send({error: true, message: error});
         }
-    });
+    });*/
 });
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
 
     const item = req.body.data;
+    var values = [];
+    values.push(item.id_item)
+
     if (!item) {
         res.status(400).send({error: true, message: 'Please provide an item'});
     } else {
-        req.getConnection((error, conn) => {
+        try {
+            const pool = await poolPromise
+            const result = await pool.request()
+                .input('id_item',sql.NVarChar,item.id_item)
+                .input('nombre',sql.NVarChar,item.nombre)
+                .input('desc',sql.NVarChar,item.descripcion)
+                .input('categoria',sql.Int,item.id_categoria)
+                .input('subcategoria',sql.Int,item.id_subcategoria)
+                .input('precio',sql.Decimal,item.precio)
+                .input('eliminado',sql.Bit,item.eliminado)
+                .input('imagen',sql.NVarChar,item.imagen)
+                .query(`INSERT INTO item VALUES (@id_item, @nombre, @desc, @categoria, @subcategoria, @precio, @eliminado, @imagen)`)
+            res.status(200).send({error: false, message: 'Item registered successfully'});
+        } catch (err) {
+            console.log(err)
+            res.status(500).send({error: true, message: err});
+        } 
+        //Vainita MySQL
+        /*req.getConnection((error, conn) => {
             if (!error) {
                 conn.query(`INSERT INTO item SET ?`, item, (err, results) => {
                     if (!err) {
@@ -121,11 +190,11 @@ app.post('/register', (req, res) => {
             } else {
                 res.status(500).send({error: true, message: error});
             }
-        });
+        });*/
     }
 });
 
-app.post('/register/combo', (req, res) => {
+app.post('/register/combo', async (req, res) => {
 
     const combo = req.body.combo;
     const itemCombo = req.body.itemCombo;
@@ -133,7 +202,30 @@ app.post('/register/combo', (req, res) => {
     if (!combo || !itemCombo) {
         res.status(400).send({error: true, message: 'Please provide combo data'});
     } else {
-        req.getConnection((error, conn) => {
+        try {
+            const pool = await poolPromise
+            const comboResult = await pool.request()
+                .input('id_combo',sql.NVarChar,combo.id_combo)
+                .input('guarnicion',sql.int,combo.max_guarnicion)
+                .input('bebida',sql.int,combo.max_bebida)
+                .query(`INSERT INTO combo VALUES (@id_combo, @guarnicion, @bebida)`)
+            var values = [];
+            itemCombo.forEach(item => {
+                var aux = [];
+                aux.push(item.id_combo)
+                aux.push(item.id_item)
+                values.push(aux)
+            });
+            const itemcomboResult = await pool.request()
+                .query('INSERT INTO itemcombo VALUES ?', [values])
+                
+            res.status(200).send({error: false, message: 'Item registered successfully'});
+        } catch (err) {
+            console.log(err)
+            res.status(500).send({error: true, message: err});
+        } 
+        //Vainita MySQL
+        /*req.getConnection((error, conn) => {
             if (!error) {
                 conn.query(`INSERT INTO combo SET ?`, combo, (err, results) => {
                     if (!err) {
@@ -153,7 +245,7 @@ app.post('/register/combo', (req, res) => {
             } else {
                 res.status(500).send({error: true, message: error});
             }
-        });
+        });*/
     }
 });
 
