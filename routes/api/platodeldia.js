6a0d4de7c77947c846d92c07e5a414b00c5cd52a
José C. Urena <20160138@ce.pucmm.edu.do>
2019-08-23 22:51:24 -0400

    platodeldia on mssql

--- routes/api/platodeldia.js
+++ routes/api/platodeldia.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const axios = require('axios');
 const url = require('../../config').values.server.url;
+const {poolPromise} = require('../../db')
+const sql = require('mssql')
 let app = express();
 
-app.get('/', (req, res, next) => {
+app.get('/', async (req, res, next) => {
 
     let sql_query = `SELECT * FROM menu WHERE plato_del_dia = 1`;
 
     sql_query += req.query.id_menu ? ` AND id_menu = ${req.query.id_menu}` : ``;
     
-    req.getConnection((error, conn) => {
+    try {
+        const pool = await poolPromise
+        const menuplatores = await pool.request()
+            .query(sql_query)
+        res.status(200).send({error: false, menu: menuplatores.recordset});
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({error: true, message: err});
+    }
+    /*req.getConnection((error, conn) => {
         if (!error) {
             conn.query(sql_query, (err, rows, fields) => {
                 if(!err) {
@@ -19,16 +30,26 @@ app.get('/', (req, res, next) => {
         } else {
             res.status(500).send({error: true, message: err});
         }
-    });
+    });*/
 });
 
-app.get('/edit/(:id_menu)', (req, res, next) => {
+app.get('/edit/(:id_menu)', async (req, res, next) => {
 
     axios.get(`${url}/api/item/get?categoria=Plato del Dia`)
     .then(items => {
         axios.get(`${url}/api/platodeldia?id_menu=${req.params.id_menu}`)
-        .then(menu => {
-            req.getConnection((error, conn) => {
+        .then(async (menu) => {
+            try {
+                const pool = await poolPromise
+                const platores = await pool.request()
+                    .input("id_menu", sql.Int, req.params.id_menu)
+                    .query(`SELECT * FROM platodeldia WHERE id_menu = @id_menu`)
+                res.status(200).send({error: false, items: items.data.items,menu: menu.data.menu[0],plato: platores.recordset});
+            } catch (err) {
+                console.log(err)
+                res.status(500).send({error: true, message: err});
+            }
+            /*req.getConnection((error, conn) => {
                 if (!error) {
                     conn.query(`SELECT * FROM platodeldia WHERE id_menu = ?`, req.params.id_menu, (err, rows, fields) => {
                         if (!err) {
@@ -38,23 +59,51 @@ app.get('/edit/(:id_menu)', (req, res, next) => {
                 } else {
                     res.status(500).send({error: true, message: err});
                 }
-            })
+            })*/
         })
         .catch(err => err);
     })
     .catch(err => console.log(err));
 });
 
-app.post('/edit/(:id_menu)', (req, res, next) => {
+app.post('/edit/(:id_menu)', async (req, res, next) => {
 
     const platos = req.body.data.platos;
-    const sql_query = `UPDATE platodeldia SET ? WHERE id_menu = ? AND dia = ?`;
+    const sql_query = ``;
     let resultados = [], errores = [];
     
     if (!platos) {
         res.status(400).send({error: true, message: 'Please provide plato del dia data'});
     } else {
-        req.getConnection((error, conn) => {
+        try {
+            const pool = await poolPromise
+            
+            res.status(200).send({error: false, menu: menuplatores.recordset});
+
+            platos.forEach(async (plate) => {
+                const platoitemres = await pool.request()
+                    .input("arroz", sql.NVarChar, plate.id_arroz)
+                    .input("habichuela",sql.NVarChar, plate.id_habichuela)
+                    .input("carne",sql.NVarChar, plate.id_carne)
+                    .input("guarnicion",sql.NVarChar, plate.id_guarnicion)
+                    .input("ensalada",sql.NVarChar, plate.id_ensalada)
+                    .input("dia",sql.Int,plate.dia)
+                    .input("menu",sql.Int,plate.id_menu)
+                    .query(`UPDATE platodeldia SET id_arroz=@arroz, id_habichuela=@habichuela, id_carne=@carne, id_guarnicion=@guarnicion, id_ensalada=@ensalada WHERE id_menu = @menu AND dia = @dia`)
+                
+            });
+
+            const menuplatores = await pool.request()
+                .input("nombre",sql.NVarChar,req.body.data.nombre)
+                .input("activo",sql.Bit,req.body.data.activo)
+                .input("id",sql.Int,req.params.id_menu)
+                .query(`UPDATE menu SET nombre = @nombre, activo = @activo WHERE id_menu = @id;`)
+            
+        } catch (err) {
+            console.log(err)
+            res.status(500).send({error: true, message: err});
+        }
+        /*req.getConnection((error, conn) => {
             if (!error) {
                 platos.forEach(plate => {
                     conn.query(sql_query, [plate, plate.id_menu, plate.dia], (err, results) => {
@@ -74,7 +123,7 @@ app.post('/edit/(:id_menu)', (req, res, next) => {
             } else {
                 res.status(500).send({error: true, message: error});
             }
-        });
+        });*/
     }
 });
 
